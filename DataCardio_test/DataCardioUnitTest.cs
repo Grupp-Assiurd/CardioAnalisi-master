5b981b02bfd92c8f092bdf008fa52cc57efcf743
Grupp-Assiurd <83281986+Grupp-Assiurd@users.noreply.github.com>
2021-05-11 12:38:09 +0200

    Update DataCardioUnitTest.cs
    Aggiunt test

--- DataCardio_test/DataCardioUnitTest.cs
+++ DataCardio_test/DataCardioUnitTest.cs
@@ -1,10 +1,23 @@
+using CardioLibrary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DataCardio_test
 {
     [TestClass]
-    public class DataCardioUnitTest
-    {  
+    public class DataCardioUnitTest 
+    {
+        [DataTestMethod]
+        [DataRow("uomo", 70, 70, 19, 50, 1428.5)]
+        [DataRow("donna", 80, 60, 60, 30, 2306.28)]
+        [DataRow("uomo", 90, 80, 40, 60, 6444.72)]
+        [DataRow("donna", 65, 50, 14, 100, 2929.55)]
+        [DataRow("uomo", 75, 40, 10, 77, 708)]
+        [DataRow("donna", 85, 64, 17, 200, 15005.33)]
 
+        public void CalorieBruciate_Test(string genere, int freq, float peso, int anni, double tempo, double risultatoEffettivo)
+        {
+            double risultatoAspettato = DataCardio.CalorieBruciate(genere, freq, peso, anni, tempo);
+            Assert.AreEqual(risultatoAspettato, risultatoEffettivo);
+        }
     }
 }

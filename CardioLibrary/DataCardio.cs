00f2cc2d3c75f81da4596f51a44629f75ca99427
Grupp-Assiurd <83281986+Grupp-Assiurd@users.noreply.github.com>
2021-05-11 13:04:26 +0200

    Update DataCardio.cs

--- CardioLibrary/DataCardio.cs
+++ CardioLibrary/DataCardio.cs
@@ -1,30 +1,26 @@
-﻿using System;
+using System;
 
 namespace CardioLibrary
 {
     public class DataCardio
     {
-        public static double CalorieBruciate( int f, float p, ushort a, uint t)
+        public static double CalorieBruciate(string genere, int f, float p, int a, double t)
         {
-
-            string genere;
-            Console.Write("Scrivi il tuo genere, uomo o donna: ");
-            genere = Console.ReadLine();
             double calcoloTempo = t * 4.184;
             double c;
             if (genere.ToLower() == "uomo")
             {
-                 c = ((a * 0.2017) + (p * 0.199) + (f * 0.6309) - 55.0969) * calcoloTempo;
+                c = ((a * 0.2017) + (p * 0.199) + (f * 0.6309) - 55.0969) * calcoloTempo;
             }
             else if (genere.ToLower() == "donna")
             {
-                 c = ((a * 0.074) + (p * 0.126) + (f * 0.4472) - 29.4022) * calcoloTempo; 
+                c = ((a * 0.074) + (p * 0.126) + (f * 0.4472) - 29.4022) * calcoloTempo;
             }
             else
             {
                 throw new Exception("Errore: genere non valido");
             }
-            return c;
+            return Math.Round(c, 2);
 
         }
 
